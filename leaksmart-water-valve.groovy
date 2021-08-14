a16ee6ffe340cd17c8c090c5c70ec91743bd154a
tmastersmart <48128988+tmastersmart@users.noreply.github.com>
2021-08-14 00:24:40 -0500

    Update leaksmart-water-valve.groovy

--- leaksmart-water-valve.groovy
+++ leaksmart-water-valve.groovy
@@ -7,16 +7,17 @@
 
 LeakSmart Valve FCC ID: W7Z-ZICM357SP2
 https://leaksmart.com/storage/2020/01/Protect-by-LeakSmart-Manual.pdf
-tested on firmware:113B-03E8-0000001D 
+
 
    https://github.com/tmastersmart/hubitat-code/blob/main/leaksmart-water-valve.groovy
    https://github.com/tmastersmart/hubitat-code/raw/main/leaksmart-water-valve.groovy
 
   Changelog:
+    2.4 08/13/2021   force battery report / cleanup   
     2.3 08/10/2021   New mains and battery detection added. Old battery detection is now EST
     2.2.2 08/10/2021  
     2.2.1 08/08/2021 Changed logging on battery routines
-    2.1 05/03/2012   Fixed log reports
+    2.1 05/03/2012   
     2.0 04/12/2021   Ported to Hubitat
 
 
@@ -66,20 +67,26 @@ metadata {
 		capability "Configuration"
 		capability "Refresh"
 		capability "Switch"
+        capability "Contact Sensor"
 		capability "Valve"
 		capability "Polling"
         capability "Power Source"
         
 		attribute "lastPoll", "number"
+        attribute "lastPollD", "number"
         attribute "batteryEST", "number"
 		attribute "batteryVoltage", "string"
+        attribute "model","string"
+        attribute "firmwareMT","string"
+        attribute "manufacture","string"
         
-        fingerprint profileId: "0104", inClusters: "0000, 0001, 0003, 0006, 0020, 0B02, FC02", outClusters: "0019", manufacturer: "WAXMAN", model: "leakSMART Water Valve v2.10", deviceJoinName: "leakSMART Valve" //leakSMART Valve
+        fingerprint profileId: "0104", inClusters: "0000, 0001, 0003, 0006, 0020, 0B02, FC02", outClusters: "0019", manufacturer: "WAXMAN", model: "leakSMART Water Valve v2.10", deviceJoinName: "leakSMART Valve v2.10" //leakSMART Valve
         fingerprint profileId: "0104", inClusters: "0000, 0001, 0003, 0004, 0005, 0006, 0008, 000F, 0020, 0B02", outClusters: "0003, 0019", manufacturer: "WAXMAN", model: "House Water Valve - MDL-TBD", deviceJoinName: "Waxman Valve" //Waxman House Water Valve
-        fingerprint profileId: "0104", inClusters: "0000, 0001, 0003, 0004, 0005, 0006, 0020, 0B02, FC02", outClusters: "0003,0019", manufacturer: "WAXMAN", model: "House Water Valve - MDL-TBD", deviceJoinName: "Leaksmart Water Valve"
+        fingerprint profileId: "0104", inClusters: "0000, 0001, 0003, 0004, 0005, 0006, 0020, 0B02, FC02", outClusters: "0003,0019", manufacturer: "WAXMAN", model: "House Water Valve - MDL-TBD", deviceJoinName: "Leaksmart Water Valve - MDL-TBD"
 		fingerprint profileId: "0104", inClusters: "0000, 0001, 0003, 0006, 0020, 0B02", outClusters: "0019"
 	}
 // need fingerprints for other valves
+// fingerprint model:"House Water Valve - MDL-TBD", manufacturer:"WAXMAN", profileId:"0104", endpointId:"01", inClusters:"0000,0001,0003,0004,0005,0006,0020,0B02,FC02", outClusters:"0003,0019", application:"1D"
 
 	preferences {
 		input "debugOutput", "bool", 
@@ -92,8 +99,8 @@ metadata {
 
 }
 private getCLUSTER_BASIC() { 0x0000 }
-private getBASIC_ATTR_POWER_SOURCE() { 0x0007 }
 private getCLUSTER_POWER() { 0x0001 }
+private getBASIC_ATTR_POWER_SOURCE() { 0x0007 }
 private getPOWER_ATTR_BATTERY_PERCENTAGE_REMAINING() { 0x0021 }
 private getTYPE_U8() { 0x20 }
 private getTYPE_ENUM8() { 0x30 }
@@ -110,17 +117,14 @@ def parse(String description) {
 //      logDebug("${device} : Parse : ${description}") 
 //
 //  This looks at all events and decodes them CATCHALL:       
-//    Map descriptionMap = zigbee.parseDescriptionAsMap(description)
-//     if (descriptionMap) {
-//      String[] receivedData = descriptionMap.data
-//       logDebug("${device} : map: ${descriptionMap} ")  
-//      logDebug("${device} : map: ${receivedData}")  
-//     logDebug("${device} : profileId: ${descriptionMap.profileId} command: ${descriptionMap.command} data: ${descriptionMap.data}")  
-//         if (descriptionMap.profileId == "0000"){
-//             def valT = 0
-//             valT = zigbee.convertHexToInt(descriptionMap.data[0])
-//             logDebug("${device} : id:0000 command: ${descriptionMap.command} value hex:${descriptionMap.data[0]} Value dec: ${valT} ")
-//         }
+    Map descriptionMap = zigbee.parseDescriptionAsMap(description)
+     if (descriptionMap) {
+      String[] receivedData = descriptionMap.data
+         if ( descriptionMap.profileId){ 
+//             logDebug("${device} : ${descriptionMap}")
+             logDebug("${device} : clusterId: ${descriptionMap.clusterId} profileId: ${descriptionMap.profileId} command: ${descriptionMap.command} data: ${descriptionMap.data}")  
+         }
+        }
 
          
          
@@ -131,28 +135,24 @@ def parse(String description) {
     if (evt) {
         logDebug "${device} :Received Event: ${evt.name} ${evt}"
         result << createEvent(name: "lastPoll",value: new Date().time, isStateChange: true, displayed: false)
-
+     	result << createEvent(name: "lastPollD", value: new Date().format("MMM dd yyyy hh:mm", location.timeZone))
 // valve status        
         if (evt.name == "switch") {
-//            logDebug "${device} :Decoding SWITCH"
 			def val2 = (evt.value == "on") ? "open" : "closed"
-//			logDebug "${device}: Valve:$val Contact:$val Switch:$evt.value"
 			result << createEvent(name: "contact", value: val2)
 			result << createEvent(name: "valve", value: val2)
-            log.info "${device}: Valve ${val2}"
 			result << createEvent(name: "switch", value: evt.value, displayed:false)
+            log.info "${device}: Valve ${val2}"
 		//	result << createEvent(name: "lastPoll",value: new Date().time, isStateChange: true, displayed: false)
 		}
         if (evt.name == "battery") {
-//            logDebug "${device} :Decoding battery"
             def val3 = evt.value
-            result << createEvent(name: "battery", value: val3)
+            result << createEvent(name: "battery", value: val3,isStateChange: true)
             log.info "${device}: battery ${val3}"
         }
         
 //  voltage status      
         if (evt.name == "batteryVoltage") {
-//            logDebug "${device} :Decoding batteryVoltage"
 //            result << createEvent(name: "batteryVoltage", value: ${evt.value}, unit:"V")
             def battest = evt.value 
 //	        def maxVolts = 6.1
@@ -178,81 +178,58 @@ def parse(String description) {
             result << createEvent(name: "powerSource", value: battery)
             log.info "${device}: powerSource: battery"
             }
-  
+            log.info "${device}: Battery ${volts} v EST${batteryLevel}%"
             result << createEvent(name: "batteryEST", value: batteryLevel, unit:"%")
-
-            
-//          logDebug "${device}: Battery ${batteryLevel}% $volts v"
-            log.info "${device}: Battery ${batteryLevel}% ${volts} v"
             result << createEvent(name: "batteryVoltage", value: volts, unit:"V")
-            
-//          result << createEvent(name: "battery", value: batteryLevel, unit:"%")
-//			result << createEvent(name: "lastPoll",value: new Date().time, isStateChange: true, displayed: false)
         }
-        // This is the mains flag
-        // Not working on my valve Reports mains all the time.
-        if (evt.name == "powerSource"){
-            // results should be Mains Battery DC Unknown
-//           logDebug "${device} :Decoding powerSource"
-            def val4 = evt.value
 
+        // This is the mains (not working on my valve?)
+        if (evt.name == "powerSource"){
+            // results should be (Mains,Battery,DC,Unknown)
+            def val4 = evt.value
             result << createEvent(name: "powerSource", value: val4)
                  log.info "${device}: powerSource: ${val4}"
-                
-        }
-  
+       }
         
-	//	result << createEvent(evt)
-	}
-
-    
+        
+	//	result << createEvent(evt)    left over this I think was to create events for unknown items
+ }
 	return result
 }
 
 
 
-def on() {
-	open()
-}
-
-def off() {
-	close()
-}
-
+def on()   {	open()  } 
+def off()  {	close() }
 def open() {
-	logDebug "${device} Opening"
+	log.info "${device} Opening the valve"
 	zigbee.on()
 }
-
 def close() {
-	logDebug "${device} Closing"
+	log.info "${device} Closing the valve"
 	zigbee.off()
 }
 
+
 def poll() {
+    
 	def minimumPollMinutes = (3 * 60) // 3 Hours
 	def lastPoll = device.currentValue("lastPoll")
 	if ((new Date().time - lastPoll) > (minimumPollMinutes * 60 * 1000)) {
-		logDebug "${device}: Poll: LastPoll was more than ${minimumPollMinutes} minutes ago."
+		logDebug "${device}: Polling: Sending refresh cmd"
+        
 		return refresh()
 	}
 	else {
-		logDebug "${device}: Poll: Skipped to soon within ${minimumPollMinutes} minutes"
+//        logDebug "${device}: Skipping Poll: must be > ${minimumPollMinutes} minutes"
+        log.info "${device}: Skipping Poll: must be > ${minimumPollMinutes} minutes"
 	}
 }
 
-//def refresh() {
-//	logDebug "${device}: Refreshing"	
-//	return zigbee.onOffRefresh() + 
-//		getBatteryReport() +
-//
-//		zigbee.onOffConfig() + 
-//
-//		configureBatteryReporting()
-//}
 
 def refresh() {
-    logDebug "${device}: Refreshing"	
+//    logDebug "${device}: Refreshing"
+    log.info "${device}: Refreshing"
     return zigbee.onOffRefresh() +
     getBatteryReport() +
     zigbee.readAttribute(CLUSTER_BASIC, BASIC_ATTR_POWER_SOURCE) +
@@ -263,22 +240,9 @@ def refresh() {
     configureBatteryReporting()    
 }
 
-
-
-
-//def configure() {
-//    
-//	logDebug "${device}: Configuring Reporting and Bindings."
-//	state.configured = true
-//	return zigbee.onOffConfig() + 
-//		configureBatteryReporting() +
-//		zigbee.onOffRefresh() +
-//       getBatteryReport()
-//}
-
 def configure() {
-    
-	logDebug "${device}: Configuring Reporting and Bindings."
+
+    logDebug "${device}: Configuring: model: ${model} firmware: ${firmwareMT} maker:${manufacture}"
 	state.configured = true
     
 
@@ -297,9 +261,9 @@ return   zigbee.onOffConfig() +configureBatteryReporting() +
 
 private configureBatteryReporting() {
 	def minSeconds = (30 * 60) // 30 Minutes
-	def maxSeconds = (3 * 60 * 60) // 3 Hours	
+	def maxSeconds = (5* 60 * 60) // 5 Hours	
 	zigbee.configureReporting(0x0001, 0x0020, 0x20, minSeconds, maxSeconds, 0x01)
-    logDebug "${device}: Set bat reports to 30 min min 3hr max"
+    logDebug "${device}: Set bat reports to 30 min min 5hr max"
 }
 
 private getSwitchReport() {
@@ -315,3 +279,11 @@ private logDebug(msg) {
 		log.debug "$msg"
 	}
 }
+
+/**
+Hubitat LeakSmart Water Valve driver
+Hubitat Iris Water Valve driver
+mains detection
+leaksmart driver hubitat
+iris water valve driver
+*/
