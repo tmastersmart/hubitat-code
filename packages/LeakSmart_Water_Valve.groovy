f5af337cc334896e870047c8b02ddef599e7ae8d
Tmaster <48128988+tmastersmart@users.noreply.github.com>
2022-09-22 00:36:28 -0500

    Create LeakSmart_Water_Valve.groovy

---
+++ packages/LeakSmart_Water_Valve.groovy
@@ -0,0 +1,24 @@
+{
+  "packageName": "Hubitat LeakSmart Water Valve",
+  "author": "Tmaster",
+  "releaseNotes": "",
+  "documentationLink": "http://www.winnfreenet.com/wp/2021/09/everspring-utilitech-water-sensor-driver-for-hubitat/",
+  "communityLink": "",
+  "version": "3.2.0",
+  "dateReleased": "2022-09-03",
+  "catagory":"Security",
+  "tags": 
+  [
+    "Safety & Security",
+    ]
+  "drivers": [
+    {
+      "name":"Hubitat LeakSmart Water Valve",
+      "namespace": "tmastersmart",
+      "location": "https://raw.githubusercontent.com/tmastersmart/hubitat-code/main/leaksmart-water-valve.groovy",
+      "required": "true",
+      "description": "Improved LeakSmart driver better battery support and better logging,Mains detection,Stuck valve detection ",
+      "id": "7b776b82-3a37-11ed-a261-0242ac120002"
+    }
+    ]
+}
