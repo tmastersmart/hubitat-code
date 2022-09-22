92e9f98e6aceb76aee323ba067a7be785bee9663
Tmaster <48128988+tmastersmart@users.noreply.github.com>
2022-09-22 00:10:02 -0500

    Create irisv1-smart-plug.groovy

---
+++ packages/irisv1-smart-plug.groovy
@@ -0,0 +1,24 @@
+{
+  "packageName": "Iris v1 Smart Plug",
+  "author": "Tmaster",
+  "releaseNotes": "",
+  "documentationLink": "",
+  "communityLink": "",
+  "version": "3.7.0",
+  "dateReleased": "2022-09-03",
+  "catagory":"Control",
+  "tags": 
+  [
+    "Misc. Devices",
+    ]
+  "drivers": [
+    {
+      "name":"Iris v1 Smart Plug",
+      "namespace": "tmastersmart",
+      "location": "https://raw.githubusercontent.com/tmastersmart/hubitat-code/main/iris_alertme_smartplug.groovy",
+      "required": "true",
+      "description": "Iris v1 Smart Plug driver. ",
+      "id": "e1b333ef-ee41-48a1-8227-69ff0ef85838"
+    }
+    ]
+}
