6527f230c39c847aec40a1528d9eeaf0e1da4ddf
Tmaster <48128988+tmastersmart@users.noreply.github.com>
2022-10-23 13:45:44 -0500

    Create zigbee relays sonoff.groovy

---
+++ packages/zigbee relays sonoff.groovy
@@ -0,0 +1,21 @@
+{
+  "packageName": "Zigbee Sonoff - generic relays",
+  "author": "TheMaster",
+  "releaseNotes":   "Working on sonoff mini.",
+  "gitHubUrl": "https://github.com/tmastersmart/hubitat-code",
+  "payPalUrl": "https://www.paypal.com/paypalme/tmastersat",	
+  "version": "1.0.1",
+  "dateReleased": "2022-10-23",
+  "category":"Control",
+  "tags":["Misc. Devices"],
+  "drivers": [
+    {
+      "name":"Zigbee Sonoff - generic relays",
+      "namespace": "tmastersmart",
+      "location": "https://raw.githubusercontent.com/tmastersmart/hubitat-code/main/generic-zigbee-relays.groovy",
+      "required": "true",
+        "description": "Works with Sonoff MINI and other generic zigbee relays",
+      "id": "e1b333ef-ee41-48a1-8227-69ff0ef85838"
+    }
+    ]
+}
