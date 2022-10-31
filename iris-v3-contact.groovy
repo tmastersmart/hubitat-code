993b9401ca3647ebef756429912310cdcbdfaee4
Tmaster <48128988+tmastersmart@users.noreply.github.com>
2022-10-31 11:15:12 -0500

    Update iris-v3-contact.groovy

--- iris-v3-contact.groovy
+++ iris-v3-contact.groovy
@@ -1,3 +1,42 @@
+/* Iris v3 contact sensor
+Iris v3 contact sensor for hubitat
+iMagic by GreatStar  model: 1116-S
+
+
+
+
+
+
+
+
+1.2.0    10/31/2021 First release
+======================================================================================
+
+Copyright [2022] [tmaster winnfreenet.com]
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+forked from 
+https://github.com/djdizzyd/hubitat/blob/320e1f5ae0ff65d7a9131ba15165b92c5afaf0be/Drivers/Iris%20V3%20Contact%20Sensor%20IL06_1/iris-V3-il06-contact-sensor.groovy
+ *	
+ */
+
+def clientVersion() {
+    TheVersion="1.2.0"
+ if (state.version != TheVersion){ 
+     state.version = TheVersion
+     configure() 
+ }
+}
+
 import hubitat.zigbee.clusters.iaszone.ZoneStatus
 
 metadata {
