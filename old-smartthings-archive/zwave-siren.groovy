516f1b253f82a5e5bc07669f1f55d9e59496dc26
Tmaster <48128988+tmastersmart@users.noreply.github.com>
2022-12-01 13:50:08 -0600

    Update zwave-siren.groovy

--- old-smartthings-archive/zwave-siren.groovy
+++ old-smartthings-archive/zwave-siren.groovy
@@ -1,5 +1,7 @@
 /**
  *  Copyright 2015 SmartThings
+ https://raw.githubusercontent.com/SmartThingsCommunity/SmartThingsPublic/master/devicetypes/smartthings/zwave-siren.src/zwave-siren.groovy
+ 
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
