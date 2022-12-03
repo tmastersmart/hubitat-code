705d5b7dff8c3f9781bbbb03b13684789b1c845d
Tmaster <48128988+tmastersmart@users.noreply.github.com>
2022-12-03 13:58:19 -0600

    Update iris_utilitech_siren_zwave.groovy

--- iris_utilitech_siren_zwave.groovy
+++ iris_utilitech_siren_zwave.groovy
@@ -1,19 +1,43 @@
 /**Iris Utilitech Siren driver hubitat
 
-
-
-
-
 This is for the Iris Utilitech Siren #0422360 No test done on other models
 Designed to detect false alarms and give proper bat suppot and drop out detection.
+Device does not support tamper. The tamper button never sends any status.
+There are no settings on this device however some EverSpring models have a timeout
+setting but im unable to test that.
+
+Presence is used to detect dead batteries. 
+
+New timeout timmer.
+
+State verification and repair. No more ignoring the OFF commands
 
 
 
+https://www.lowes.com/pdf/UT_IndoorSiren_IM%208-2%20ENG-101512.pdf
+------------------------------------------------------------------------------------------------------
+Its sometimes hard to get it to pair or repair after removing or resetting.
+Remove batteries let it set and try again later.
+
+UNDOCUMENTED HUBITAT Z-WAVE BUG
+Go to settings/Z-Wave Details/Z-Wave Radio Devices
+
+Unlike other zwave devices this device gets stuck in this list and the hub gets confused.
+GENERIC_TYPE_SWITCH_BINARY Everspring
+If its already in zwave list it will never be able to re-pair. Press REPAIR or REFREASH and you will 
+get a option to REMOVE, Remove it then the hub will now see it as a new device.
+I beleive the cause is that normal devices creating a new ID on each pair. Whatever the case the hub does
+not know what to do. I think it was said after several reboots it will go away but we need it out now.
+---------------------------------------------------------------------------------------------------------
 
 
- v1.1.0    12/01/2022  Phase 2 creation rewriting code
+ v1.0.5    12/03/2022 First working release
+ v1.0.1    12/01/2022  beta
  v1.0.0    10/01/2022  created 
        
+
+
+
 
 
  *  Copyright 2022 by winnfreenet.com
@@ -26,7 +50,9 @@ Designed to detect false alarms and give proper bat suppot and drop out detectio
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
-
+ *
+ *  using zwave commands from this driver. Code rewritten......
+ *  https://github.com/SmartThingsCommunity/SmartThingsPublic/blob/master/devicetypes/smartthings/zwave-siren.src/zwave-siren.groovy
  */
 metadata {
 	definition(name: "Iris Utilitech Siren", namespace: "tmastersmart", author: "tmaster",importUrl: "https://raw.githubusercontent.com/tmastersmart/hubitat-code/main/iris_utilitech_siren_zwave.groovy") {
@@ -34,9 +60,7 @@ metadata {
 		capability "Alarm"
 		capability "Sensor"
 		capability "Switch"
-		capability "Tamper Alert"
 		capability "Health Check"
-        
         capability "Battery"
 		capability "Configuration"
         capability "Refresh"
@@ -45,7 +69,7 @@ metadata {
         
         command "checkPresence"
         command "initialize"
-        
+        command "uninstall"
 
         fingerprint mfr: "0060", prod: "000C", deviceId:"0001", deviceJoinName: "Utilitech Siren" , inClusters:"0x20,0x25,0x86,0x80,0x85,0x72,0x71"
 		fingerprint mfr: "0060", prod: "000C", model: "0001", deviceJoinName: "Utilitech Siren" 
@@ -60,22 +84,45 @@ preferences {
 	input name: "debugLogging", type: "bool", title: "Enable debug logging", description: "MED level Debug" ,defaultValue: false,required: true
 	input name: "traceLogging", type: "bool", title: "Enable trace logging", description: "Insane HIGH level", defaultValue: false,required: true
 
+    input name: "timeout" ,type: "enum", title: "Timeout",description: "Auto send off command and reset after x seconds 0=disabled 300=5min 1800=30min",options: ["0","300","600","1200","1500","1800"], defaultValue: 1800 ,required: true 
+
+    input name: "pollYes",type: "bool", title: "Enable Presence", description: "", defaultValue: true,required: true
+    input name: "pollHR" ,type: "enum", title: "Check Presence Hours",description: "Press config after saving",options: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"], defaultValue: 8 ,required: true 
+
 
 }
 def clientVersion() {
-    TheVersion="1.2.0"
+    TheVersion="1.0.5"
  if (state.version != TheVersion){ 
      state.version = TheVersion
      configure() 
  }
 }
 
+def uninstall() {
+ 
 
+    unschedule()
+    state.icon = ""
+    state.donate = ""
+    state.remove("presenceUpdated")    
+	state.remove("version")
+    state.remove("configured")
+    state.remove("lastCheckInMin")
+    state.remove("icon")
+    state.remove("logo")  
+    state.remove("FalseAlarm")
+    state.remove("tries")
+    state.remove("lastPoll")
+    state.remove("Alarm")        
+
+      
+      }
 
 def installed() {
 	logging("${device} : Paired!", "info")
     initialize()
-    runIn(20,refresh)
+
 }
 
 
@@ -84,7 +131,7 @@ def updated() {
     logging("updated","info")
     clientVersion()
 	loggingUpdate()
-	state.configured = false
+    if (pollYes == false){unschedule(checkPresence)}
 }
 
 
@@ -94,14 +141,6 @@ def initialize() {
     state.FalseAlarm = 0
     
     
- delayBetween([
-        zwave.basicV1.basicGet().format(),
-        zwave.associationV2.associationSet(groupingIdentifier:1, nodeId:zwaveHubNodeId).format(),
-        zwave.wakeUpV2.wakeUpIntervalGet().format(),
-        zwave.batteryV1.batteryGet().format(),
-    ], 2300)
-
-
 configure()
 
 }
@@ -111,15 +150,29 @@ configure()
 
 def configure() {
 	logging("Config","info")
-	state.configured = true
-    unschedule()
+    state.Alarm = false
+    if(!state.FalseAlarm){state.FalseAlarm = 0}
     
-    // Schedule presence in hrs 8
+    state.remove("configured")
+    state.remove("LastCheckin")
+    state.remove("initializeAttempts")
+    state.remove("lastAlarmType")
+    removeDataValue("manufacturerName")
+    getIcons()
+    
+    unschedule(checkPresence)
+    if (pollYes){ 
 	randomSixty = Math.abs(new Random().nextInt() % 60)
+    randomSixty2 = Math.abs(new Random().nextInt() % 60)    
 	randomTwentyFour = Math.abs(new Random().nextInt() % 24)
-	schedule("${randomSixty} ${randomSixty} ${randomTwentyFour}/${8} * * ? *", checkPresence)	
-    logging("Presence set for 8 hrs ${randomSixty} ${randomSixty} ${randomTwentyFour}/${8} * * ? *", "info")	
+    logging("CHRON: ${randomSixty2} ${randomSixty} ${randomTwentyFour}/${pollHR} * * ? *", "debug") 
+    schedule("${randomSixty2} ${randomSixty} ${randomTwentyFour}/${pollHR} * * ? *", checkPresence)	
+    logging("Presence Check Every ${pollHR}hrs starting at ${randomTwentyFour}:${randomSixty}:${randomSixty2} ", "info") 
+    } 
+    
 
+    
+    
 //Everspring 1,2,3 minutes	1:60, 2:120, 3:180 (untested this is a IRIS driver)
  
 	if (zwaveInfo?.mfr == "0060" && zwaveInfo?.prod == "000C" && zwaveInfo?.model == "0002") {// 0001= Utilitech  0002=Everspring
@@ -133,25 +186,57 @@ def configure() {
     ], 2300)
 }
 
-    
+runIn(20,refresh)    
 }
 
+// Resend the proper state to get back in sync
+def sync (){
+    state.FalseAlarm = state.FalseAlarm +1 // Keeping track of how many we get
+    logging("Resyncing State. Errors:${state.FalseAlarm}", "warn") 
+    if (state.Alarm== true){ on()}
+    else {off()}
+}
 
 def on() {
-    logging("On", "info")
+    logging("Sending On", "info")
     state.Alarm = true
+
+    runIn(20,ping) 
+    if (timeout == 300){ runIn(300,off)}
+    if (timeout == 600){ runIn(600,off)}
+    if (timeout == 800){ runIn(800,off)}
+    if (timeout == 1500){ runIn(1500,off)}
+    if (timeout == 1800){ runIn(1800,off)}
+    if (timeout == 2200){ runIn(2200,off)}
+
     delayBetween([
-        secure(zwave.basicV1.basicSet(value: 0xFF)),
-        secure(zwave.basicV1.basicGet())
-    ], 3000)
+        zwave.basicV1.basicSet(value: 0xFF).format(),
+        zwave.switchBinaryV1.switchBinaryGet().format(),
+ 
+       secure(zwave.basicV1.basicSet(value: 0xFF)),
+       secure(zwave.switchBinaryV1.switchBinaryGet()),
+       secure(zwave.basicV1.basicGet())
+    ], 3000) 
+    
+
+}
+
+def offOFF(){
+off()
 }
 
 
 def off() {
-    logging("Off", "info")
+    logging("Sending Off", "info")
     state.Alarm = false
+    runIn(20,poll)
+    
    delayBetween([
+        zwave.basicV1.basicSet(value: 0x00).format(),
+        zwave.switchBinaryV1.switchBinaryGet().format(),
+       
         secure(zwave.basicV1.basicSet(value: 0x00)),
+        secure(zwave.switchBinaryV1.switchBinaryGet()),
         secure(zwave.basicV1.basicGet())
     ], 3000)
 }
@@ -176,12 +261,14 @@ def both() {
 // -------------------------------------------------
 def poll() {
     logging("Poll", "info") 
-    zwave.basicV1.basicGet().format()
+//    zwave.basicV1.basicGet().format()
+    zwave.switchBinaryV1.switchBinaryGet().format()
 }
 
 def ping() {
     logging("Ping", "info") 
-    zwave.basicV1.basicGet().format()
+//    zwave.basicV1.basicGet().format()
+    zwave.switchBinaryV1.switchBinaryGet().format()
 }
 
 def refresh() {
@@ -193,10 +280,12 @@ def refresh() {
 
     
     delayBetween([
-//        zwave.configurationV2.configurationGet().format(),
-        zwave.basicV1.basicGet().format(),
+//      zwave.configurationV2.configurationGet().format(),
+        zwave.manufacturerSpecificV2.manufacturerSpecificGet().format(),// fingerprint
+//        zwave.basicV1.basicGet().format(),// this just dupes switchbinary
         zwave.batteryV1.batteryGet().format(),
-		secure(zwave.batteryV1.batteryGet())
+        zwave.switchBinaryV1.switchBinaryGet().format(),
+
     ], 3000)
 }
 
@@ -245,15 +334,11 @@ def checkPresence() {
 
 def parse(String description) {
     def nowCal = Calendar.getInstance(location.timeZone)
-    Timecheck = "${nowCal.getTime().format("EEE MM/dd/YYYY", location.timeZone)}"
     state.lastCheckin = now()
-    state.LastCheckin = Timecheck
-    logging("${device} : Raw [${description}]", "trace")
+    logging("Raw [${description}]", "trace")
     checkPresence()
 
-    CommandClassCapabilities = [0x20: 1,0x9C: 1, 0x71: 1, 0x84: 2, 0x30: 1, 0x70: 1]  
-//    CommandClassCapabilities = [0x20: 1, 0x71: 1]
-    hubitat.zwave.Command map = zwave.parse(description, CommandClassCapabilities)
+    hubitat.zwave.Command map = zwave.parse(description, getCommandClassVersions())
     if (map == null) {return null}
     def result = [map]
     if (!result) {return null}
@@ -265,6 +350,46 @@ def parse(String description) {
     }
 
 }
+
+/**
+ *  getCommandClassVersions()
+ *
+ *  Returns a map of the command class versions supported by the device. Used by parse() and zwaveEvent() to
+ *  extract encapsulated commands from MultiChannelCmdEncap, MultiInstanceCmdEncap, SecurityMessageEncapsulation,
+ *  and Crc16Encap messages.
+ *
+ *  Reference: http://products.z-wavealliance.org/products/629/classes
+ **/
+private getCommandClassVersions() {
+    return [
+        0x20: 1, // Basic V1
+        0x22: 1, // Application Status V1 (Not advertised but still sent)
+        0x25: 1, // Switch Binary V1
+        0x27: 1, // Switch All V1
+        0x30: 1, // ?
+        0x32: 3, // Meter V3
+        0x56: 1, // CRC16 Encapsulation V1
+        0x70: 1, // Configuration V1
+        0x71: 1, // Alarm (Notification) V1
+        0x72: 2, // Manufacturer Specific V2
+        0x75: 2, // Protection V2
+        0x80: 1, // Battery v1
+        0x84: 2, // ?
+        0x85: 2, // Association V2
+        0x86: 1, // Version V1
+        0x87: 1, // Indicator V1
+        0x9C: 1 // Alarm Sensor
+    ]
+}
+
+//The siren supports Z-Wave™ Command Classes including:
+// * COMMAND_CLASS_BASIC
+// * COMMAND_CLASS_VERSION
+// * COMMAND_CLASS_BATTERY
+// * COMMAND_CLASS_MANUFACTURER_SPECIFIC
+// * COMMAND_CLASS_SWITCH_BINARY
+// * COMMAND_CLASS_ASSOCIATION_V2
+
 
 private secure(hubitat.zwave.Command cmd) {
 	if (zwaveInfo?.zw?.contains("s")) {
@@ -283,105 +408,158 @@ def zwaveEvent(hubitat.zwave.commands.securityv1.SecurityMessageEncapsulation cm
 	}
 }
 
-def zwaveEvent(hubitat.zwave.commands.configurationv2.ConfigurationReport cmd) {
-    logging("configuration report: ${cmd}", "debug")
-}
 
-def zwaveEvent(hubitat.zwave.commands.notificationv3.NotificationReport cmd) {
-    logging("NotificationReport: ${cmd}", "debug")
-}
 
-// This is the report we receive <----------------------------------------
-def zwaveEvent(hubitat.zwave.commands.basicv1.BasicReport cmd) {
-    logging("${cmd}", "debug")
-    
-    if (cmd.value == 0){  value="off"} 
-    if (cmd.value == 255){value="on"} 
+void readSwitch (cmd){
+    if (cmd == 0){  value="off"} 
+    if (cmd == 255){value="on"} 
     
     if (value == "on" || value=="off"){
-    logging("Event ${value} ", "info")
+        logging("State:${value} [Ours:${state.Alarm}]", "info")
         sendEvent(name: "switch", value: value, descriptionText: " v${state.version}", isStateChange:true)
         sendEvent(name: "alarm",  value: value, descriptionText: " v${state.version}", isStateChange:true)
         sendEvent(name: "strobe", value: value, descriptionText: " v${state.version}", isStateChange:true) 
+        // Error recovery     
+        if (value == "on"  && state.Alarm == false){sync()}  
+        if (value == "off" && state.Alarm == true ){sync()} 
+        if (value == "off" && state.Alarm == false){unschedule(off)}// stop the auto off timmer
     }
-	
-//  An atempt to stop the random false alarms the device gets.
-    if (value == "on" && state.Alarm == false){
-        logging("False Alarm, ON not sent Turning OFF", "warn")  
-        off()
-        state.FalseAlarm = state.FalseAlarm +1 // Keeping track of how many we get
-    }  
-        
-        
-}
 
+
+
+}
+// SwitchBinaryReport(value:0) (only when we request status )
 def zwaveEvent(hubitat.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd) {
     logging("${cmd}", "debug")
-	
+    readSwitch(cmd.value)
 }
+
+// zwaveEvent( COMMAND_CLASS_BASIC (0x20)  v1 ) (normaly reports here)
+def zwaveEvent(hubitat.zwave.commands.basicv1.BasicReport cmd) {
+    logging("${cmd}", "debug")
+    readSwitch(cmd.value)
+}
+
+/**
+ *  zwaveEvent( COMMAND_CLASS_BATTERY V1 (0x80) : BATTERY_REPORT (0x03) )
+ *
+ *
+ *  Example: BatteryReport(batteryLevel: 52)
+ **/
 
 
 def zwaveEvent(hubitat.zwave.commands.batteryv1.BatteryReport cmd) {
-      logging("received batteryv1 ${cmd}", "debug")
+      logging("${cmd}", "debug")
     if (cmd.batteryLevel == 0xFF) { 
-        logging("Low battery FLAG FF", "warn")// not getting these reports
+        logging("Low battery FLAG FF", "warn")
         return
      } 
     if (cmd.batteryLevel == 0) { 
-        logging("Ignoring bat 0 ", "warn")
+        logging("Ignoring false bat 0 ", "debug")
         return
      } 
     
-//    state.LastBat = device.currentValue("battery")
-//    if (state.LastBat != cmd.batteryLevel ){ 
-    logging("battery: ${cmd.batteryLevel} ", "info")
-    sendEvent(name: "battery", value: cmd.batteryLevel,unit: "%", descriptionText: "${cmd.batteryLevel} ${state.LastCheckin} v${state.version}", isStateChange:true)
-//    }
+    state.LastBat = device.currentValue("battery")
+    if (state.LastBat != cmd.batteryLevel ){ 
+     logging("battery: ${cmd.batteryLevel} ", "info")
+     sendEvent(name: "battery", value: cmd.batteryLevel,unit: "%", descriptionText: "${cmd.batteryLevel} ${state.LastCheckin} v${state.version}", isStateChange:true)
+    }
 }
 
 
+/**
+ *  zwaveEvent( COMMAND_CLASS_MANUFACTURER_SPECIFIC V2 (0x72) : MANUFACTURER_SPECIFIC_REPORT (0x05) )
+ *
+ *  Manufacturer-Specific Reports are used to advertise manufacturer-specific information, such as product number
+ *  and serial number.
+ **/
+def zwaveEvent(hubitat.zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd) {
+  logging("${cmd} ", "debug")
+    
+  // does not send name in text  
+   def map = [:]
+    map.mfr   = hubitat.helper.HexUtils.integerToHexString(cmd.manufacturerId, 2)
+    map.model = hubitat.helper.HexUtils.integerToHexString(cmd.productId, 2)
+    map.type  = hubitat.helper.HexUtils.integerToHexString(cmd.productTypeId, 2)
+    state.fingerprint = "${map.mfr}-${map.type}-${map.model}"
+    logging("fingerprint ${state.fingerprint}", "debug")
+    
+    
+    updateDataValue("manufacturer","Iris Utilitech")
+    updateDataValue("manufacturerId","${cmd.manufacturerId}")
+    updateDataValue("deviceType","${cmd.productTypeId}")
+    updateDataValue("model","TSE07-1")  
+    updateDataValue("deviceId","${cmd.productId}") 
+    
+}
+    
 
-
-
-
-
-
-// these are untrapped log them...
-
-def zwaveEvent(hubitat.zwave.Command cmd) {
+/**
+ *  zwaveEvent( COMMAND_CLASS_ALARM (0x71) : ALARM_REPORT (0x05) )
+ *
+ *  The Alarm Report command used to report the type and level of an alarm.
+ *
+ *  cmd attributes:
+ *    Short  alarmLevel  Application specific
+ *    Short  alarmType   Application specific
+ **/
+def zwaveEvent(hubitat.zwave.commands.alarmv1.AlarmReport cmd) {
 	logging("${cmd} ", "debug")
-    if (cmd.alarmLevel == 0 && cmd.alarmType==0){return}// why send us 0 0 
-// AlarmReport(alarmLevel:1, alarmType:2) Unknown report received on bat change.
+    
+    if (cmd.alarmLevel == 0 && cmd.alarmType==0){return}// why send us 0 0 AllClear?
+
     if (cmd.alarmLevel == 1 && cmd.alarmType==2){
-        logging("Powering Up OK", "info")
+        logging("Alarm Type 2 - Powering Up OK", "info")
         return
     }
+    // Way to many false alarms here (unusable) Just log for now
     if (cmd.alarmLevel == 1 && cmd.alarmType==1){
-        logging("Low Battery Alarm", "info")// some of these are false
+        logging("Alarm Type 1 - Battery Low", "debug")
         return
     }
        
-    logging("Unknown Level:${cmd.alarmLevel} Type:${cmd.alarmType}", "info")
+    logging("Unknown Alarm Level:${cmd.alarmLevel} Type:${cmd.alarmType}", "warn")
 }
 
 
 
 
 
+def zwaveEvent(hubitat.zwave.Command cmd) {
+	logging("${cmd} ", "debug")
+
+}
 
 
 
 
+void getIcons(){
+    state.icon ="<img src='https://raw.githubusercontent.com/tmastersmart/hubitat-code/main/images/utilitech-siren.jpg' >"    
+    state.donate="<a href='https://www.paypal.com/paypalme/tmastersat?locale.x=en_US'><img src='https://raw.githubusercontent.com/tmastersmart/hubitat-code/main/images/paypal2.gif'></a>"
 
 
-// Logging block 
-//	device.updateSetting("infoLogging",[value:"true",type:"bool"])
+ }
+
+
+
+
+// Logging block  v5
 void loggingUpdate() {
-    logging("${device} : Logging Info:[${infoLogging}] Debug:[${debugLogging}] Trace:[${traceLogging}]", "infoBypass")
-    // Only do this when its needed
-    if (debugLogging){runIn(3600,debugLogOff)}
-    if (traceLogging){runIn(1800,traceLogOff)}
+    logging("Logging Info:[${infoLogging}] Debug:[${debugLogging}] Trace:[${traceLogging}]", "infoBypass")
+        if (debugLogging){
+        logging("Debug log:off in 3000s", "warn")
+        runIn(3000,debugLogOff)
+    }
+    if (traceLogging){
+        logging("Trace log: off in 1800s", "warn")
+        runIn(1800,traceLogOff)
+    }
 }
+
+void loggingCheck(){ 
+// not working fix later
+}
+
 void traceLogOff(){
 	device.updateSetting("traceLogging",[value:"false",type:"bool"])
 	log.trace "${device} : Trace Logging : Automatically Disabled"
