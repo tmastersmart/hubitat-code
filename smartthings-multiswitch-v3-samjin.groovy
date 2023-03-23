a9b1816312a325d9a2794fee43a36a326abc2e5d
Tmaster <48128988+tmastersmart@users.noreply.github.com>
2023-03-23 18:26:26 -0500

    Update smartthings-multiswitch-v3-samjin.groovy

--- smartthings-multiswitch-v3-samjin.groovy
+++ smartthings-multiswitch-v3-samjin.groovy
@@ -22,7 +22,7 @@ To go back to internal drivers without removing use uninstall then change driver
 
 
 ===================================================================================================
-1.4.0    03/23/2023 Hub update broke automatic min volt test. Moved to a setting
+1.4.1    03/23/2023 Hub update broke automatic min volt test. Moved to a setting
 1.3.9    01/25/2023 Power Up routine init rewriten
 1.3.8    01/23/2023 Bat code event rewrite
 1.3.7    12/20/2022 Debugging work
@@ -55,7 +55,7 @@ import hubitat.zigbee.zcl.DataType
 import hubitat.helper.HexUtils
 
 def clientVersion() {
-    TheVersion="1.4.0"
+    TheVersion="1.4.1"
 if (state.version != TheVersion){
     logging("Upgrading ! ${state.version} to ${TheVersion}", "warn")
      state.version = TheVersion
@@ -112,7 +112,7 @@ preferences {
    input name: "pingIt" ,type: "enum", title: "Ping Time",description: "Ping every x mins. Press config after saving",options: ["5","10","15","20","25","30"], defaultValue: "10",required:true
    input name: "tempAdj",type: "enum", title: "Temperature Offset",description: "", options: ["-10","-9.8","-9.6","-9.4","-9.2","-9.0","-8.8","-8.6","-8.4","-8.2","-8.0","-7.8", "-7.6","-7.4","-7.2","-7.0","-6.8","-6.6","-6.4","-6.2","-6.0","-5.8","-5.6","-5.4","-5.2","-5.0","-4.8","-4.6","-4.4","-4.2","-4.0","-3.8","-3.6","-3.4","-3.2","-3.0","-2.8","-2.6","-2.4","-2.2","-2.0","-1.8","-1.6","-1.4","-1.2","-1.0","-0.8","-0.6","-0.4","-0.2","0",    "0.2","0.4","0.6","0.8","1.0","1.2","1.4","1.6","1.8","2.0","2.2","2.4","2.6","2.8","3.0","3.2","3.4","3.6","3.8","4.0","4.2","4.4","4.6","4.8","5.0","5.2","5.4","5.6","5.8",   "6.0","6.2","6.4","6.6","6.8","7.0","7.2","7.4","7.6","7.8","8.0","8.2","8.4","8.6","8.8","9.0","9.2","9.4","9.6","9.8","10"], defaultValue: 0 ,required: true  
 
-   input name: "minVoff",type: "enum", title: "Min Voltage",description: "Using minVoltTest set the min voltage your sensor will run on", options: ["1.9","2.0","2.1","2.2","2.3","2.4","2.5"], defaultValue: 2.2 ,required: true  
+   input name: "minVoff",type: "enum", title: "Min Voltage",description: "Using minVoltTest set the min voltage your sensor will run on", options: ["1.9","2","2.1","2.2","2.3","2.4","2.5"], defaultValue: "2.2" ,required: true  
    input name: "pollYes",type: "bool", title: "Enable Presence", description: "", defaultValue: true,required: true
    input name: "pollHR" ,type: "enum", title: "Check Presence Hours",description: "Press config after saving",options: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"], defaultValue: 8 ,required: true 
 
@@ -337,14 +337,22 @@ if (evt.name == "batteryVoltage"){//Event: [name:batteryVoltage, value:2.9]
         def  powerLast = device.currentValue("battery")
         def  batVolts  = device.currentValue("batteryVoltage")
         def  minVolts  = 2.2 
-        def  maxVolts  = 3
+        def  maxVolts  = 2.9
+    
+
+
+    if (minVoff == "1.9"){minVolts = 1.9 }
+    if (minVoff == "2.0"){minVolts = 2 }
+    if (minVoff == "2.1"){minVolts = 2.1 }
+    if (minVoff == "2.2"){minVolts = 2.2 }
+    if (minVoff == "2.3"){minVolts = 2.3 }
+    if (minVoff == "2.4"){minVolts = 2.4 }
+    if (minVoff == "2.5"){minVolts = 2.5 }
     
        if (state.MFR == "SmartThings") {
 			minVolts = 15 // this is defaut from st driver untested
 			maxVolts = 28
-          }    
-
-    if (minVoff){minVolts = minVoff} // this replaces automatic setting
+          }        
     
 // hub update broke this.   in testing 2.4 is min on some sensors. 2.1 is the min on others 
 //        if(state.minVoltTest){minVolts = state.minVoltTest} // this should hold the lowest voltage if set
@@ -597,6 +605,7 @@ if (state.MFR == "Samjin"){state.icon ="<img src='https://raw.githubusercontent.
     state.donate="<a href='https://www.paypal.com/paypalme/tmastersat?locale.x=en_US'><img src='https://raw.githubusercontent.com/tmastersmart/hubitat-code/main/images/paypal2.gif'></a>"
 
 }
+
 
 
 // Logging block  v4
